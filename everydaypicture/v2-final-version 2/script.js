3961c63e3e6777004042437d04a56846e44860ba
Phoenix Chen <phxchen@ucdavis.edu>
2026-05-26 05:04:16 -0700

    update

--- everydaypicture/v2-final-version 2/script.js
+++ everydaypicture/v2-final-version 2/script.js
@@ -6,6 +6,7 @@
     const endOverlay = document.getElementById("end-overlay");
     const resetBtn = document.getElementById("reset-btn");
 
+    // image, positioning, and the respective text
     const inventory = [
         { img: "images/apartment.jpeg", x: 70, y: 40, story: "July 9, 2025. I was standing outside on my aunt's balcony and it felt like the perfect moment to take a photo. My aunt has around 25 cats that she's rescued and I think I want to make a little booklet of them with their picture, name, and backstories the next chance I get." },
         { img: "images/lake.jpg", x: 70, y: 40, story: "July 11, 2025. Hac Sa Beach. On this evening, I spent quite a bit of time meandering around on the coast of Coloane in Macau. I remember this was the day before I attended Irene and Seulgi's concert and it felt so special because I quite literally traveled across the world to see them. Though my trip wasn't purely for that, I was really happy that they coincided with my plans." },
@@ -13,6 +14,7 @@
         { img: "images/street.jpeg", x: 70, y: 40, story: "Juy 7, 2025. I was on my way back to my aunt's apartment after meeting up with my dad's side of the family after a long period of time. We went to yumcha and afterwards I hung out with my cousin. for the first time. Even though she's 38, she was somehow super up-to-date with media culture. Within a few hours, we were able to connect really well and she shared with me her yaoi tablet (where I learned that she was a total fujoshi)." },
     ];
 
+    // camera interaction
     let isAnimating = false;
     let currentIndex = 0;
 
@@ -56,6 +58,7 @@
         }, 300);
     });
 
+    // close all
     resetBtn.addEventListener("click", function () {
         endOverlay.style.display = "none";
 
@@ -70,7 +73,7 @@
             }, index * 40);
         });
 
-
+        // reset counter
         setTimeout(() => {
             items.forEach(el => el.remove());
             currentIndex = 0;
